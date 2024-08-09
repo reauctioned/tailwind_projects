ab8395178ba83337fe550478c87d596a7f60f646
Saurav <sauravgamer441@gmail.com>
2024-08-09 23:28:13 +0545

    + tailwind functional verified

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function App() {
 
   return (
     <>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      
     </>
   )
-
+}
 
 export default App
