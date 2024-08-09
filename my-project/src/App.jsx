067ad8f4442e7afa6e14146db6a89a428333e05f
Saurav <sauravgamer441@gmail.com>
2024-08-09 23:39:21 +0545

    + chitchat component + basic tailwind css

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ChitChat from './components/chitchat'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <ChitChat/>
       
     </>
   )
